94155833aadf04a757e283214286daa45a61af70
yoshi-koyama <koyama@reytech.co.jp>
2020-03-22 13:28:36 +0900

    fixed conflict by koyama

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -8,18 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-<<<<<<< HEAD:demo/src/main/java/com/example/demo/HomeController.java
-public class HomeController {
-=======
 public class HelloController {
->>>>>>> a40e93ba4a41a703242d7bac09bab7ccc9405281:demo/src/main/java/com/example/demo/HelloController.java
 
-public static void main(String[] args) {
-SpringApplication.run(HomeController.class, args);
-}
+    public static void main(String[] args) {
+    SpringApplication.run(HomeController.class, args);
+    }
 
-@GetMapping("/hello")
-public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-return String.format("Hello %s!", name);
-}
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return String.format("Hello %s!", name);
+    }
 }
