7956ca4cf41388ebb7ad43272aa0522bbded0f73
yoshi-koyama <koyama@reytech.co.jp>
2020-03-22 13:45:23 +0900

    refactoring by koyama

--- demo/src/main/java/com/example/demo/HomeController.java
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -2,20 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@RestController
+@Controller
 public class HomeController {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HomeController.class, args);
-	}
-
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	public String hello(Model model) {
+		model.addAttribute("message", "hello world");
+		return "hello";
 	}
 }
